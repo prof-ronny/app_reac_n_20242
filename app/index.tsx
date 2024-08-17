40f2c0b1d59005109a054cf050409316df493343
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2024-08-17 01:04:06 +0000

    Criação Estado e Função Map

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,64 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+
+const produtos = [
+  { id: 1, nome: "Coca-Cola", preco: 5.5 },
+  { id: 2, nome: "Pepsi", preco: 5.5 },
+  { id: 3, nome: "Fanta", preco: 5.5 },
+  { id: 4, nome: "Guaraná", preco: 5.5 },
+];
+
+
 
 export default function Index() {
+  let [contador,setContador]= useState(0);  
+
   return (
     <View
       style={estilo.container}
     >
-      <Text style={estilo.text} >Olá Mundo</Text>
+      {produtos.map((p)=> (
+        <View key={p.id} >
+          <Text style={estilo.titulo}>{p.nome} </Text>
+          <Text>{p.preco}</Text>
+        </View>
+      )
+     )}
+     <Button title={contador.toString()}
+      onPress={()=>{clicarBotao()}} ></Button>
+
+      
     </View>
   );
+
+  function clicarBotao(){
+    setContador(contador+1);
+  
+  }
 }
+
+
 
 const estilo= StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FF0000",
+    alignItems: "flex-start",
+    backgroundColor: "#FFFFFF",
+    paddingStart:20
+    
   },
   text: {
     color: "#FFFFFF",
     fontSize: 20,
   },
+  titulo:{
+    color: "#000000",
+    fontSize: 30,
+    textAlign: "left",
+    fontWeight: "bold"
+
+  },
+
+
 });
